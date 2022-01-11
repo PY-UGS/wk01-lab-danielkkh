d28e3e12b557cb7b64a02dee495bfc51287f7faa
danielkkh <97492854+danielkkh@users.noreply.github.com>
2022-01-11 14:25:29 +0800

    Add files via upload

---
+++ Switch.java
@@ -0,0 +1,14 @@
+public class Switch 
+{
+    public static void main(String arg[]) 
+ { 
+    String module_code = "CSC1009"; 
+    switch(module_code) 
+    { 
+        case "CSC1009": 
+        System.out.println("Objected-Oriented Programming");
+        break; 
+    } 
+ System.out.println("After switch"); 
+ } 
+}
